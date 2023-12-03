a68c16218f3745e84b93ed8f70b4fc4c8191b5db
AxelAlvardo <00216022@uca.edu.sv>
2023-12-02 22:55:51 -0600

    fixed Landing-img

--- BACKPACK-APP/src/components/Page/Footer/Footer.jsx
+++ BACKPACK-APP/src/components/Page/Footer/Footer.jsx
@@ -10,21 +10,21 @@ export const Footer = () => {
                 <NavLink to="/login" className="footer__cta">Empezar</NavLink>
             </div>            
        
-            <img src="../../../public/images/bubble-1.png" alt="buble" className="bubble bubble-1" />
-            <img src="../../../public/images/bubble-3.png" alt="buble" className="bubble bubble-2" />
-            <img src="../../../public/images/bubble-3.png" alt="buble" className="bubble bubble-3" />
-            <img src="../../../public/images/bubble-2.png" alt="buble" className="bubble bubble-4"/>
-            <img src="../../../public/images/bubble-2.png" alt="buble" className="bubble bubble-5"/>
-            <img src="../../../public/images/bubble-5.png" alt="buble" className="bubble bubble-6" />
-            <img src="../../../public/images/bubble-5.png" alt="buble" className="bubble bubble-7" />
+            <img src="/images/bubble-1.png" alt="buble" className="bubble bubble-1" />
+            <img src="/images/bubble-3.png" alt="buble" className="bubble bubble-2" />
+            <img src="/images/bubble-3.png" alt="buble" className="bubble bubble-3" />
+            <img src="/images/bubble-2.png" alt="buble" className="bubble bubble-4"/>
+            <img src="/images/bubble-2.png" alt="buble" className="bubble bubble-5"/>
+            <img src="/images/bubble-5.png" alt="buble" className="bubble bubble-6" />
+            <img src="/images/bubble-5.png" alt="buble" className="bubble bubble-7" />
        
        
         </div>
 
         <figure className='images__section'>
-                <img src="../../../public/images/laptop-poses-2.png" className='img woman' alt="footer__img" />
-                <img src="../../../public/images/chat-group-2.png" className='img chat' alt="footer__img" />
-                <img src="../../../public/images/men-mobile-2.png" className='img man' alt="footer__img" />
+                <img src="/images/laptop-poses-2.png" className='img woman' alt="footer__img" />
+                <img src="/images/chat-group-2.png" className='img chat' alt="footer__img" />
+                <img src="/images/men-mobile-2.png" className='img man' alt="footer__img" />
         </figure>
 
         <FooterEnd/>       
